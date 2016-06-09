f712754a56ae12c3dd51f917b9816b1712c9d7e1
marcelinojgm <marcelino.j.granda@gmail.com>
2016-06-09 12:43:59 +0200

    creado tarjetas

--- app/src/main/java/com/example/marcelino/sqlite/ListasContract.java
+++ app/src/main/java/com/example/marcelino/sqlite/ListasContract.java
@@ -9,15 +9,15 @@ import android.provider.BaseColumns;
  */
 public final class ListasContract  {
     public ListasContract(){}
-    /** clase interna que define los contenidos de la tabla**/
-    public static abstract class lista implements BaseColumns{
+
+    public static abstract class ColumnLista implements BaseColumns {
         public static final String TABLE_NAME = "Listas";
         public static final String COLUMN_NAME_ENTRY_ID = "lista_id";
         public static final String COLUMN_NAME_TITULO   = "titulo";
 
     }
 
-    public static abstract class elementos implements BaseColumns{
+    public static abstract class ColumnElementos implements BaseColumns {
         public static final String TABLE_NAME = "Elementos";
         public static final String COLUMN_NAME_LISTA_ID = "lista_id";
         public static final String COLUMN_NAME_ELEMENTO_ID = "elemento_id";
--- app/src/main/java/com/example/marcelino/sqlite/ListasReaderDbHelper.java
+++ app/src/main/java/com/example/marcelino/sqlite/ListasReaderDbHelper.java
@@ -6,13 +6,26 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class ListasReaderDbHelper extends SQLiteOpenHelper {
 
-    public ListasReaderDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String INTEGER_TYPE = " INTEGER";
+    private static final String COMMA_SEP = ",";
+
+    public ListasReaderDbHelper(Context context, String name,
+                                SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        private static final String TEXT_TYPE = " TEXT";
+        private static final String COMMA_SEP = ",";
+        private static final String SQL_CREATE_ENTRIES =
+                "CREATE TABLE " + Tabla.ColumnasLista.TABLE_NAME + " (" +
+                        FeedEntry._ID + " INTEGER PRIMARY KEY," +
+                        FeedEntry.COLUMN_NAME_ENTRY_ID + TEXT_TYPE + COMMA_SEP +
+                        FeedEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +
+        ... // Any other options for the CREATE command
+        " )";
     }
 
     @Override
